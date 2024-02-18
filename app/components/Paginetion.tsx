741afc66a471e5fd016f75ecaf1bddd427cb82e8
shihoin2 <merysousi@gmail.comm>
2024-02-18 23:04:45 +0900

    ページネーションの変更

--- app/components/Paginetion.tsx
+++ app/components/Paginetion.tsx
@@ -1,24 +1,24 @@
-// "use client"
-// import Link from 'next/link'
+"use client"
+import Link from 'next/link'
 
-// export default function Pagination({ pageData }) {
-//   return(
-//     <>
-//       <div>
-//         <ul className="pagination">
-//           {pageData.links.map(link => (
-//             <li key={link.label} className={`page-item ${link.active ? 'active' : ''}`}>
-//               {link.url ? (
-//                 <Link href={link.url}>
-//                   <a className="page-link">{link.label}</a>
-//                 </Link>
-//               ) : (
-//                 <span className="page-link">{link.label}</span>
-//               )}
-//             </li>
-//           ))}
-//         </ul>
-//       </div>
-//       </>
-//     );
-// }
+export default function Pagination({ pageData }:{pageData:any}) {
+  return(
+    <>
+      <div>
+        <ul className="pagination">
+          {pageData.links.map((link:any) => (
+            <li key={link.label} className={`page-item ${link.active ? 'active' : ''}`}>
+              {link.url ? (
+                <Link href={link.url}>
+                  <a className="page-link">{link.label}</a>
+                </Link>
+              ) : (
+                <span className="page-link">{link.label}</span>
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
+      </>
+    );
+}
